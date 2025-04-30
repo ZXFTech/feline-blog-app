05987b4041b067e8cd4b8f4700b114b848c58ab5
Feline <zxf_tech@outlook.com>
2025-04-30 18:04:54 +0800

    icon 引入方式调整

--- src/components/Icon/presetIcon.tsx
+++ src/components/Icon/presetIcon.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
 
 import SvgNeonCat from "../../../public/neoncat.svg";
+// import NextSvg from "../../../public/next.svg";
 import Icon, { IconProps } from "./icon";
+import NeuDiv from "../NeuDiv/NeuDiv";
+import Image from "next/image";
+import Link from "next/link";
 
 export const IconSpinner: FC<Partial<IconProps>> = (props) => {
   // return <Icon icon={icon || "spinner"} {...restProps} />;
@@ -11,8 +15,16 @@ export const IconSpinner: FC<Partial<IconProps>> = (props) => {
 
 export const IconNeonCat: FC = () => {
   return (
-    // <NeuDiv neuType="embossed" className="icon-neon-cat">
-    <SvgNeonCat className="neon-light" />
-    // </NeuDiv>
+    <NeuDiv neuType="embossed" className="cursor-pointer icon-neon-cat">
+      <Link href="/">
+        <Image
+          src="/neoncat.svg"
+          alt=""
+          width={48}
+          height={48}
+          className="w-10 neon-light"
+        />
+      </Link>
+    </NeuDiv>
   );
 };

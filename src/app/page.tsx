70dd2e2dad3b2877fd5c691a12dceefc9a35a777
Feline <zxf_tech@outlook.com>
2025-04-30 16:10:48 +0800

    测试页内容同步

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
-import Image from "next/image";
+import NeuButton from "@/components/NeuButton/neuButton";
+import Button from "../components/Button/button";
+import Icon from "../components/Icon/icon";
+import NeuDiv from "@/components/NeuDiv/NeuDiv";
 
 export default function Home() {
-  return <div className="neu-light">首页</div>;
+  return (
+    <NeuDiv className="neu-light">
+      <NeuDiv className="flex items-center gap-2">
+        <span className="text-sm">icon:</span>
+        <Icon theme="danger" icon="search" />
+      </NeuDiv>
+      <NeuDiv className="flex items-center gap-2">
+        <span className="text-sm">button:</span>
+        <Button icon="search" loading>
+          测试
+        </Button>
+        <NeuButton icon="search" loading>
+          测试
+        </NeuButton>
+      </NeuDiv>
+      <NeuDiv neuType="debossed" className="flex items-center gap-2">
+        <span className="text-sm">button:</span>
+        <Button icon="search" loading>
+          测试
+        </Button>
+        <NeuButton icon="search" loading>
+          测试
+        </NeuButton>
+      </NeuDiv>
+    </NeuDiv>
+  );
 }

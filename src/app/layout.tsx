33c46cd405b24d47f2f9710fe5e0b2c91748b8e7
Feline <zxf_tech@outlook.com>
2025-04-30 15:20:24 +0800

    添加全局 sass 样式文件

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "material-symbols";
-import "./globals.css";
+import "../styles/index.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

c043abce508926961ed883e43029b90a8226b46b
Feline <zxf_tech@outlook.com>
2025-05-07 10:56:25 +0800

    首页测试代码增加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@ import NeuButton from "@/components/NeuButton/neuButton";
 import Button from "../components/Button/button";
 import Icon from "../components/Icon/icon";
 import NeuDiv from "@/components/NeuDiv/NeuDiv";
+import Tag from "@/components/Tag/tag";
 
 export default function Home() {
   return (
     <NeuDiv className="neu-light">
-      <NeuDiv className="flex items-center gap-2">
+      <NeuDiv className="flex flex-wrap items-center gap-2">
         <span className="text-sm">icon:</span>
         <Icon theme="danger" icon="search" />
       </NeuDiv>
-      <NeuDiv className="flex items-center gap-2">
+      <NeuDiv className="flex flex-wrap items-center gap-2">
         <span className="text-sm">button:</span>
         <Button icon="search" loading>
           测试
@@ -19,7 +20,7 @@ export default function Home() {
           测试
         </NeuButton>
       </NeuDiv>
-      <NeuDiv neuType="debossed" className="flex items-center gap-2">
+      <NeuDiv neuType="debossed" className="flex flex-wrap items-center gap-2">
         <span className="text-sm">button:</span>
         <Button icon="search" loading>
           测试
@@ -27,6 +28,20 @@ export default function Home() {
         <NeuButton icon="search" loading>
           测试
         </NeuButton>
+      </NeuDiv>
+      <NeuDiv neuType="debossed" className="flex flex-wrap items-center gap-2">
+        <span className="text-sm">tag:</span>
+        <Tag icon="search">测试</Tag>
+        <Tag color="green">测试</Tag>
+        <Tag icon="search" color="red" closable>
+          测试
+        </Tag>
+        <Tag icon="search" color="red">
+          测试
+        </Tag>
+        <Tag icon="search" color="red">
+          测试
+        </Tag>
       </NeuDiv>
     </NeuDiv>
   );

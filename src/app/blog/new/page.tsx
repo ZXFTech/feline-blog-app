20f5878c809cc114167463fa76296533741c474a
Feline <zxf_tech@outlook.com>
2025-05-07 22:11:49 +0800

    添加新建博客页及新建博客按钮

---
+++ src/app/blog/new/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import MarkdownEditor from "@/components/MarkdownEditor/markdownEditor";
+
+const New = () => {
+  return (
+    <div className="mt-7">
+      <MarkdownEditor />
+    </div>
+  );
+};
+
+export default New;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import BlogList from "@/components/BlogList/blogList";
 import { Metadata } from "next";
 import { dataSource } from "./data";
+import NeuDiv from "@/components/NeuDiv/NeuDiv";
+import NeuButton from "@/components/NeuButton/neuButton";
+import Link from "next/link";
 
 export function Blog() {
   return (
     <div className="mt-7">
+      <NeuDiv className="top-panel w-[95%] mx-[auto] flex flex-wrap">
+        <Link href="/blog/new" className="hover:no-underline!">
+          <NeuButton className="">新建</NeuButton>
+        </Link>
+      </NeuDiv>
       <BlogList
         dataSource={dataSource as any}
         pageBean={{ pageNum: 0, pageSize: 20, total: 5 }}

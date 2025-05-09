10cc34a9f8efce1d3f9c62e30d7e07dedefcc665
Feline <zxf_tech@outlook.com>
2025-05-09 22:47:00 +0800

    博客详情页获取博客内容逻辑添加

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,92 +1,104 @@
+"use server";
+
 import Head from "next/head";
 
-import { GetStaticPaths, GetStaticProps } from "next";
-import { NotionPage } from "@/lib/notion/types";
 import NeuDiv from "@/components/NeuDiv/NeuDiv";
 import Tag from "@/components/Tag/tag";
 
-import NotionBlock from "@/components/NotionBlock/notionBlock";
+import NotionBlock, { CodeBlock } from "@/components/NotionBlock/notionBlock";
 
 import Image from "next/image";
 
-import { blogData } from "./data";
+import { getBlogById } from "@/db/blogAction";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import Content from "@/components/Content/content";
+import NeuButton from "@/components/NeuButton/neuButton";
 
-const PostLayout = ({
-  blogRes,
-  contentRes,
-  origin = "notion",
-}: {
-  blogRes: NotionPage;
-  contentRes: any;
-  origin: any;
-}) => {
-  // if (!blogRes && !contentRes) {
-  //   return <div></div>;
-  // }
-  const { last_edited_time, properties } = blogData.pageProps.blogRes;
-  const { Page, authors, tags } = properties;
+// export async function getStaticProps({ params }: { params: { id: number } }) {
+//   const postData = await getBlogById(params.id);
+//   console.log("postData", postData);
+// }
 
-  const contentList: any[] =
-    (blogData.pageProps.contentRes?.results as any[]) || [];
+const Blog = async ({ params }: { params: Promise<{ id: number }> }) => {
+  const { id } = await params;
+  const postData = await getBlogById(id);
+
+  if (!postData) {
+    return (
+      <div className="blog-empty-content mt-7">
+        <NeuDiv className="blog-empty-title">
+          这篇博客没有内容, 所以给你看看我的小猫吧~
+        </NeuDiv>
+        <NeuDiv className="blog-empty-image">
+          <div>
+            <Image
+              layout="responsive"
+              src="/img/myCat.jpg"
+              alt="my cat"
+              width={100}
+              height={100}
+              objectFit="cover"
+            />
+          </div>
+        </NeuDiv>
+      </div>
+    );
+  }
+
   return (
-    <div className="px-[5%] pt-10 pb-15 border-x overflow-auto h-[calc(100vh-5rem)] hidden-scrollbar">
-      <Head>
-        <title>{Page.title.map((item) => item.plain_text)}</title>
-      </Head>
-      <h1>{Page.title.map((item) => item.plain_text)}</h1>
-      <div className="flex flex-wrap items-center justify-between">
-        <Tag className="ml-0">
-          {authors.people.map((author) => author.name).join("/")}
-        </Tag>
-        {tags.multi_select.length && (
+    <Content>
+      <div className="operation-bar">
+        <NeuButton icon="edit">编辑</NeuButton>
+      </div>
+      <NeuDiv
+        neuType="flat"
+        className="blog-content-container mt-3 p-5 border-inherit overflow-auto"
+      >
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <h1>{postData.title}</h1>
+        <div className="flex flex-wrap items-center justify-between mb-3">
+          <Tag className="ml-0">{postData.author.username}</Tag>
+          {/* {(postData.TagsOnBlog||[]).length && (
           <ul className="flex flex-wrap gap-1 p-0 mx-0 my-3">
-            {origin && <Tag>来源: {origin}</Tag>}
-            {tags.multi_select.map((tag) => {
+            {postData.TagsOnBlog.map((tag) => {
               return (
                 <Tag color={tag.color} key={tag.id}>
                   {tag.name}
                 </Tag>
               );
             })}
-            {last_edited_time && (
-              <Tag>{(new Date(last_edited_time), "yyyy-MM-dd HH:mm:ss")}</Tag>
-            )}
           </ul>
-        )}
-      </div>
-      <div className="text-left">
-        {!contentList.length ? (
-          <div className="blog-empty-content">
-            <NeuDiv className="blog-empty-title">
-              这篇博客没有内容, 所以给你看看我的小猫吧~
-            </NeuDiv>
-            <NeuDiv className="blog-empty-image">
-              <div>
-                <Image
-                  layout="responsive"
-                  src="/img/myCat.jpg"
-                  alt="my cat"
-                  width={100}
-                  height={100}
-                  objectFit="cover"
-                />
-              </div>
-            </NeuDiv>
-          </div>
-        ) : (
-          contentList.map((content) => {
-            if (content.type in NotionBlock) {
-              return (
-                <section className="">
-                  {(NotionBlock as any)[content.type](content)}
-                </section>
-              );
-            }
-          })
-        )}
-      </div>
-    </div>
+        )} */}
+        </div>
+        <div className="blog-content text-left">
+          <ReactMarkdown
+            components={{
+              code({ node, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return match ? (
+                  <CodeBlock
+                    title="test"
+                    code={children ? children.toString() : ""}
+                    language={match[1]}
+                  ></CodeBlock>
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
+            remarkPlugins={[remarkGfm]}
+          >
+            {JSON.parse(postData.content)}
+          </ReactMarkdown>
+        </div>
+      </NeuDiv>
+    </Content>
   );
 };
 
-export default PostLayout;
+export default Blog;

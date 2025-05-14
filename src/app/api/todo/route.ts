4c6d1c779b025dd6ecce09557953e80970f2e809
Feline <zxf_tech@outlook.com>
2025-05-14 22:03:16 +0800

    增加 todo 相关 api

---
+++ src/app/api/todo/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { addTodo, getTodoList, updateTodo } from "@/db/todoAction";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  // 获取所有 Todo
+  return NextResponse.json(await getTodoList());
+}
+export async function POST(req: NextRequest, res: NextResponse) {
+  // 创建新 Todo
+  const { content } = await req.json();
+  return NextResponse.json(await addTodo(content));
+}
+
+export async function PATCH(req: NextRequest, res: NextResponse) {
+  // 更新 Todo
+  const data = await req.json();
+  console.log("data", data);
+  return NextResponse.json(await updateTodo(data));
+}

c1161cb15619678705b8d0387fdeac1ae9d139a4
Feline <zxf_tech@outlook.com>
2025-05-16 23:16:46 +0800

    todo 编辑弹窗逻辑添加

--- src/components/Todo/TodoEditorBar.tsx
+++ src/components/Todo/TodoEditorBar.tsx
@@ -5,59 +5,79 @@ import NeuButton from "../NeuButton/neuButton";
 import dynamic from "next/dynamic";
 import { message } from "@/lib/message";
 import { useRouter } from "next/navigation";
+import NeuInput from "../NeuInput";
+import { Todo } from "../../../generated/prisma";
+import Icon from "../Icon/icon";
 
 const Model = dynamic(() => import("../Modal"), { ssr: false });
 
 const TodoEditorBar = ({ refresh }: { refresh: () => void }) => {
-  const router = useRouter();
   const [visible, setVisible] = useState<boolean>(false);
+  const [loading, setLoading] = useState(false);
+  const [validateMessage, setValidateMessage] = useState("");
+
+  const [todoData, setTodoData] = useState<Partial<Todo>>({
+    content: "",
+    finished: false,
+  });
+
+  const onDataChange = (key: string, value: string) => {
+    setTodoData({
+      ...todoData,
+      [key]: value,
+    });
+  };
 
   const handleClose = () => {
     setVisible(false);
   };
 
-  const data = {
-    content:
-      "测试todo everybody farmer society plastic motion thy noted market outline that brave cattle welcome silver trip mass becoming father prevent equator ago task fill coach",
-  };
-
-  // useEffect(() => {
-  //   if (visible) {
-  //     const timer = setTimeout(() => {
-  //       setVisible(false);
-  //     }, 1000);
-  //     return () => clearTimeout(timer);
-  //   }
-  // }, [visible]);
-
   const addTodo = async () => {
+    setLoading(true);
     const res = await fetch("/api/todo", {
       method: "POST",
-      body: JSON.stringify(data),
+      body: JSON.stringify(todoData),
     });
 
-    console.log("res", res);
-
     const result = await res.json();
+    setLoading(false);
     if (result.error) {
       message.error(result.message);
+      return;
     }
+    message.success("添加成功!");
+    setVisible(false);
     refresh();
-    // router.replace("/todo");
   };
 
-  const handleOk = () => {
-    setVisible(false);
+  const handleOk = async () => {
+    if (!todoData.content?.trim()) {
+      setValidateMessage("待办内容不能为空!");
+      return;
+    }
+    validateMessage && setValidateMessage("");
+    await addTodo();
   };
-  const handleCreateNew = () => {};
   return (
     <div>
-      <NeuButton onClick={() => addTodo()}>添加</NeuButton>
-      <Model visible={visible} onClose={handleClose} onOk={handleOk}>
-        <NeuButton>测试</NeuButton>
-        <NeuButton>测试</NeuButton>
-        <NeuButton>测试</NeuButton>
-        <NeuButton>测试</NeuButton>
+      <NeuButton className="p-1!" onClick={() => setVisible(true)}>
+        <Icon icon="note_add" size="3xl" />
+      </NeuButton>
+      <Model
+        okLoading={loading}
+        visible={visible}
+        onClose={handleClose}
+        onOk={handleOk}
+      >
+        <NeuInput
+          disabled={loading}
+          className="w-full"
+          value={todoData.content}
+          onChange={(value: any) => onDataChange("content", value.target.value)}
+        />
+        {validateMessage && (
+          <span className="text-red-500">{validateMessage}</span>
+        )}
       </Model>
       {/* <TestModal visible={visible} onClose={handleClose}>
         <NeuButton>测试</NeuButton>

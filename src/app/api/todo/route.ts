b6a2ecc302181b6755d63d69102ca3a9f1c9a357
Feline <zxf_tech@outlook.com>
2025-05-21 15:04:24 +0800

    修复 next api route 参数错误, 去除未使用的 api

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -2,17 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { addTodo, getTodoList, updateTodo } from "@/db/todoAction";
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest, params: never) {
   // 获取所有 Todo
   return NextResponse.json(await getTodoList());
 }
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   // 创建新 Todo
   const { content } = await req.json();
   return NextResponse.json(await addTodo(content));
 }
 
-export async function PATCH(req: NextRequest, res: NextResponse) {
+export async function PATCH(req: NextRequest) {
   // 更新 Todo
   const data = await req.json();
   return NextResponse.json(await updateTodo(data));

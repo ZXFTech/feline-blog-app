56781611824e16c91754d527e3ef0555e10343ee
Feline <zxf_tech@outlook.com>
2025-05-26 20:40:44 +0800

    添加构建及node-ts 相关配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // /* config options here */
+  distDir: "/dist/.next",
+  cleanDistDir: true,
   images: {
     remotePatterns: [
       {

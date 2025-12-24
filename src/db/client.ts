eb6695d50f1eb3e5bfe04b11469bb4fd11edca93
Feline <zxf_tech@outlook.com>
2025-12-24 15:59:26 +0800

    配置启动环境变量，配置 prisma client

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,8 +1,18 @@
-import { PrismaClient } from "../../generated/prisma";
+import "dotenv/config";
+import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
+});
 
 const globalForDB = globalThis as unknown as { db: PrismaClient };
 
-const db = globalForDB.db || new PrismaClient();
+const db = globalForDB.db || new PrismaClient({ adapter });
 
 export default db;
 

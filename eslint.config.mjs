090cf09af96d24f2af58e8bca838de6a0dba0c6b
Feline <zxf_tech@outlook.com>
2025-12-26 15:11:01 +0800

    next & eslint 配置升级

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,18 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default defineConfig([
-  {
-    extends: compat.extends("next/core-web-vitals", "next/typescript"),
-  },
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
   globalIgnores([
-    ".next/**/*", // ignore next build file
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
     "generated/prisma", // ignore prisma generate
     "node_modules",
   ]),
 ]);
+
+export default eslintConfig;

b1e84b6aea1e9fa874eb9a796fb266b26eba9c26
Feline <zxf_tech@outlook.com>
2026-01-01 14:01:09 +0800

    checkUser 支持 password 字段自定义

--- src/db/userAction.ts
+++ src/db/userAction.ts
@@ -2,10 +2,22 @@
 
 import db from "./client";
 
-const checkUser = async (identifier: "id" | "email", payload: string) => {
+const checkUser = async (
+  identifier: "id" | "email",
+  payload: string,
+  password = false
+) => {
   const user = await db.user.findFirst({
     where: {
       [identifier]: payload,
+    },
+    select: {
+      id: true,
+      email: true,
+      username: true,
+      role: true,
+      avatar: true,
+      password,
     },
   });
   return user;

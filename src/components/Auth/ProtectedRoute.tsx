231d88815ca14bd6b006c5a74073567cf041d533
Feline <zxf_tech@outlook.com>
2026-01-02 20:04:59 +0800

    页面权限控制组件

---
+++ src/components/Auth/ProtectedRoute.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useAuth } from "@/hooks/useAuth";
+import { ReactNode, useEffect, useState } from "react";
+import { toast as message } from "@/components/ProMessage";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  children: ReactNode;
+  requiredRole?: string[];
+}
+
+export const ProtectRoute = ({ children, requiredRole }: Props) => {
+  const { loading, isAdmin, isRoot, isAuthenticated, user } = useAuth();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !isAuthenticated) {
+      message.warning("未登录或登录过期, 请先登录.");
+      router.push("/login");
+    }
+
+    if (!loading && isAuthenticated && requiredRole) {
+      const hasRequiredRole = requiredRole.includes(user!.role);
+      if (!hasRequiredRole) {
+        router.push("/unauthorized");
+      }
+    }
+  }, [loading, isAuthenticated, user, router, requiredRole]);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return null;
+  }
+
+  if (requiredRole && requiredRole.includes(user!.role)) {
+    return null;
+  }
+
+  return <>{children}</>;
+};

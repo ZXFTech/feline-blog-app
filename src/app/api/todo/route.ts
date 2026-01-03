c7d828d42b9415ae1eee05c89f3113c6cae7b363
Feline <zxf_tech@outlook.com>
2026-01-03 23:23:43 +0800

    调整 /api/todo 方法返回值

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 import { addTodo, deleteTodo, getTodoList, updateTodo } from "@/db/todoAction";
 
@@ -13,29 +13,29 @@ export async function GET(req: NextRequest) {
       : false
     : null;
 
+  const res = await getTodoList({
+    orderBy,
+    content,
+    finished,
+  });
+
   // 获取所有 Todo
-  return NextResponse.json(
-    await getTodoList({
-      orderBy,
-      content,
-      finished,
-    })
-  );
+  return res;
 }
 export async function POST(req: NextRequest) {
   // 创建新 Todo
   const data = await req.json();
-  return NextResponse.json(await addTodo(data));
+  return await addTodo(data);
 }
 
 export async function PATCH(req: NextRequest) {
   // 更新 Todo
   const data = await req.json();
-  return NextResponse.json(await updateTodo(data));
+  return await updateTodo(data);
 }
 
 export async function DELETE(req: NextRequest) {
   // 删除 todo
   const { todoId } = await req.json();
-  return NextResponse.json(await deleteTodo(todoId));
+  return await deleteTodo(todoId);
 }

91b6bcd851fc125935a27d06ea9b13d710778584
Feline <zxf_tech@outlook.com>
2026-01-03 23:24:15 +0800

    修复 permission access 组件样式和逻辑判断问题

--- src/components/Auth/PermissionAccess.tsx
+++ src/components/Auth/PermissionAccess.tsx
@@ -16,13 +16,18 @@ export const PermissionAccess = ({
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center min-h-screen">
+      <div className="flex items-center justify-center ">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
       </div>
     );
   }
 
-  if (!loading && !user && !allowedRoles.includes(user!.role)) {
+  if (
+    !loading &&
+    !user &&
+    allowedRoles?.length &&
+    allowedRoles.includes(user!.role)
+  ) {
     return <>{fallbacks}</>;
   }
 

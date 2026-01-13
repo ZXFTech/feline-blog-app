4ee22c85a581767652267cde5f74855bf17b263c
Feline <zxf_tech@outlook.com>
2026-01-13 18:13:30 +0800

    content 布局样式调整

--- src/components/Content/content.tsx
+++ src/components/Content/content.tsx
@@ -20,11 +20,11 @@ const Content: FC<ContentProps> = ({
       {leftSideBar && (
         <div className="hidden lh:block w-[20%]]">{leftSideBar}</div>
       )}
-      <div className="w-2xl md:w-2xl xl:w-2xl h-[100vh] overflow-scroll hide-scrollbar pt-19 pb-10 relative">
+      <div className="w-2xl md:w-2xl xl:w-2xl h-[100vh] overflow-scroll hide-scrollbar pt-22 pb-10 relative">
         {children}
       </div>
       {rightSideBar && (
-        <div className="hidden md:block w-[25%] h-[100vh] overflow-scroll hide-scrollbar pt-19 pb-10 px-4">
+        <div className="hidden md:block w-[25%] h-[100vh] overflow-scroll hide-scrollbar pt-22 pb-10 px-4">
           {rightSideBar}
         </div>
       )}

3ba8755fe8e749cbd6552e52b6e995f558081699
Feline <zxf_tech@outlook.com>
2026-01-14 19:15:52 +0800

    修复 NeuButton line height 异常的问题

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -47,7 +47,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
     "btn",
     "inline-flex items-center justify-center",
     `${children ? "gap-1" : "gap-0"}`,
-    `${children ? "leading-5!" : "leading-0"}`,
+    !children && "p-1!",
     {
       [`btn-${buttonType}`]: buttonType,
       [`btn-${btnSize}`]: btnSize,

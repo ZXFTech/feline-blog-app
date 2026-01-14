d93347c5a727397d85dd316017510d8ee3dda52a
Feline <zxf_tech@outlook.com>
2026-01-14 13:31:06 +0800

    todo item 组件拆分

--- src/components/Todo/TodoDatePart.tsx
+++ src/components/Todo/TodoDatePart.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import { TagTodo } from "@/types/todo";
-import Icon from "../Icon";
-import NeuButton from "../NeuButton";
-import NeuDiv from "../NeuDiv";
 import Tag from "../Tag";
+import TodoItem from "./TodoItem";
 
 interface Props {
   dateKey: string;
@@ -26,58 +24,13 @@ const TodoDatePart = ({
       <Tag>{new Date(dateKey).toLocaleDateString("zh-CN")}</Tag>
       {todoList.map((todo) => {
         return (
-          <div key={todo.id} className="flex items-start justify-center gap-2">
-            <NeuDiv
-              style={{ fontSize: "20px" }}
-              className="p-1! m-0! leading-0"
-            >
-              {todo.finished ? (
-                <Icon
-                  className="text-green-700"
-                  size="xl"
-                  icon="select_check_box"
-                />
-              ) : (
-                <Icon size="xl" className="" icon="check_box_outline_blank" />
-              )}
-            </NeuDiv>
-            <NeuButton
-              onClick={() => handleClick(todo)}
-              className={`m-0! w-full overflow-hidden! p-3! block! text-start`}
-            >
-              <span
-                className={`${
-                  todo.finished ? "line-through" : ""
-                } text-sm text-start whitespace-break-spaces font-semibold inline-block`}
-              >
-                {todo.content}
-              </span>
-              {todo.tags?.length ? (
-                <div className="todo-tags flex flex-wrap gap-1 justify-end mt-2">
-                  {todo.tags.map((item, index) => {
-                    const tag = item.tag;
-                    return (
-                      <Tag key={tag.content + index} color={tag.color}>
-                        {tag.content}
-                      </Tag>
-                    );
-                  })}
-                </div>
-              ) : null}
-            </NeuButton>
-            <div className="flex flex-col">
-              <NeuButton
-                className="p-2! mb-2"
-                icon="edit"
-                onClick={() => handleUpdate(todo)}
-              ></NeuButton>
-              <NeuButton
-                className="p-2!"
-                icon="delete"
-                onClick={() => handleDelete(todo.id!)}
-              ></NeuButton>
-            </div>
-          </div>
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onTodoClick={handleClick}
+            onTodoDelete={handleDelete}
+            onTodoUpdate={handleUpdate}
+          />
         );
       })}
     </div>
---
+++ src/components/Todo/TodoItem.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Icon from "../Icon";
+import NeuButton from "../NeuButton";
+import NeuDiv from "../NeuDiv";
+import Tag from "../Tag";
+import { TagTodo } from "@/types/todo";
+
+interface Props {
+  todo: TagTodo;
+  onTodoClick: (todo: TagTodo) => void;
+  onTodoUpdate: (todo: TagTodo) => void;
+  onTodoDelete: (todoId: number) => void;
+}
+
+function TodoItem({ todo, onTodoClick, onTodoDelete, onTodoUpdate }: Props) {
+  return (
+    <div className="flex items-start justify-center gap-2">
+      <NeuDiv style={{ fontSize: "20px" }} className="p-1! m-0! leading-0">
+        {todo.finished ? (
+          <Icon className="text-green-700" size="xl" icon="select_check_box" />
+        ) : (
+          <Icon size="xl" className="" icon="check_box_outline_blank" />
+        )}
+      </NeuDiv>
+      <NeuButton
+        onClick={() => onTodoClick(todo)}
+        className={`m-0! w-full overflow-hidden! p-3! block! text-start`}
+      >
+        <span
+          className={`${
+            todo.finished ? "line-through" : ""
+          } text-sm text-start whitespace-break-spaces font-semibold inline-block`}
+        >
+          {todo.content}
+        </span>
+        {todo.tags?.length ? (
+          <div className="todo-tags flex flex-wrap gap-1 justify-end mt-2">
+            {todo.tags.map((tag, index) => {
+              return (
+                <Tag key={tag.content + index} color={tag.color}>
+                  {tag.content}
+                </Tag>
+              );
+            })}
+          </div>
+        ) : null}
+      </NeuButton>
+      <div className="flex flex-col">
+        <NeuButton
+          className="p-2! mb-2"
+          icon="edit"
+          onClick={() => onTodoUpdate(todo)}
+        ></NeuButton>
+        <NeuButton
+          className="p-2!"
+          icon="delete"
+          onClick={() => onTodoDelete(todo.id!)}
+        ></NeuButton>
+      </div>
+    </div>
+  );
+}
+
+export default TodoItem;

4f800fef6202b604e4a8c259c74c31e305cd4930
Feline <zxf_tech@outlook.com>
2026-01-18 11:06:14 +0800

    svg 图标添加

---
+++ src/components/Logo/svg/index.ts
@@ -0,0 +1,7 @@
+export { default as LogoReact } from "./react-dark.svg";
+export { default as LogoNextjs } from "./nextjs.svg";
+export { default as LogoVue } from "./vue.svg";
+export { default as LogoNodejs } from "./nodejs.svg";
+export { default as LogoTailwind } from "./tailwindcss.svg";
+export { default as LogoSass } from "./sass.svg";
+export { default as LogoMysql } from "./mysql.svg";

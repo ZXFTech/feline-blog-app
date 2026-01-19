d278f8ecbf937bc5ed055ca260df1d8b1c61bdb8
Feline <zxf_tech@outlook.com>
2026-01-19 15:50:30 +0800

    Flip Clock 样式调整

--- src/components/Clock/FlipClock.tsx
+++ src/components/Clock/FlipClock.tsx
@@ -7,6 +7,16 @@ function pad(n: number) {
   return String(n).padStart(2, "0");
 }
 
+const weekDayMap: { [key: number]: string } = {
+  0: "一",
+  1: "二",
+  2: "三",
+  3: "四",
+  4: "五",
+  5: "六",
+  6: "七",
+};
+
 export default function FlipClock() {
   const { time } = useClock();
 
@@ -17,29 +27,35 @@ export default function FlipClock() {
   const [m1, m2] = pad(time.getMinutes()).split("");
   const [s1, s2] = pad(time.getSeconds()).split("");
 
-  console.log("m1", M2);
+  const weekDay = weekDayMap[time.getDay()];
 
   return (
-    <div className="flip-clock">
-      <FlipDigit value={y1} />
-      <FlipDigit value={y2} />
-      <FlipDigit value={y3} />
-      <FlipDigit value={y4} />
-      <span className="separator">-</span>
-      <FlipDigit value={M1} />
-      <FlipDigit value={M2} />
-      <span className="separator">-</span>
-      <FlipDigit value={D1} />
-      <FlipDigit value={D2} />
-      <span className="separator"> </span>
-      <FlipDigit value={h1} />
-      <FlipDigit value={h2} />
-      <span className="separator">:</span>
-      <FlipDigit value={m1} />
-      <FlipDigit value={m2} />
-      <span className="separator">:</span>
-      <FlipDigit value={s1} />
-      <FlipDigit value={s2} />
+    <div>
+      <div className="flip-clock year-part">
+        <div>{y1}</div>
+        <div>{y2}</div>
+        <div>{y3}</div>
+        <div>{y4}</div>
+        <span className="separator">-</span>
+        <div>{M1}</div>
+        <div>{M2}</div>
+        <span className="separator">-</span>
+        <div>{D1}</div>
+        <div>{D2}</div>
+        <span className="separator"> </span>
+        <div>星期</div>
+        <div>{weekDay}</div>
+      </div>
+      <div className="flip-clock">
+        <FlipDigit value={h1} />
+        <FlipDigit value={h2} />
+        <span className="separator">:</span>
+        <FlipDigit value={m1} />
+        <FlipDigit value={m2} />
+        <span className="separator">:</span>
+        <FlipDigit value={s1} />
+        <FlipDigit value={s2} />
+      </div>
     </div>
   );
 }

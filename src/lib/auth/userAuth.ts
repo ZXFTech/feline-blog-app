e6b3b007f64c65fb1cbdb9ca4f676c3fbf555231
Feline <zxf_tech@outlook.com>
2026-01-25 20:23:55 +0800

    todo action 增加角色权限校验

--- src/lib/auth/userAuth.ts
+++ src/lib/auth/userAuth.ts
@@ -2,6 +2,7 @@ import { verifyToken } from "../jwt";
 import { checkUser } from "@/db/userAction";
 import logger from "../logger/Logger";
 import { getCookieData } from "../cookieStore";
+import { Role } from "../../../generated/prisma/enums";
 
 export async function getCurrentUser() {
   try {
@@ -32,4 +33,14 @@ export async function requireAuth() {
     throw "用户未登录";
   }
   return user;
+}
+
+export async function hasTodoRoles() {
+  const todoRoles: Role[] = [Role.ROOT];
+  const user = await requireAuth();
+  if (todoRoles.includes(user.role)) {
+    return user;
+  }
+
+  throw "无 Todo 编辑权限";
 }

5da5918ee8863cdac29bce142444e370db3623f6
Feline <zxf_tech@outlook.com>
2026-02-06 16:09:19 +0800

    番茄时钟增加 日历 记录展示

--- src/app/tomato/page.tsx
+++ src/app/tomato/page.tsx
@@ -1,16 +1,34 @@
-import Content from "@/components/Content";
+import Calendar, { RecordDate } from "@/components/Calendar";
 import { Pomodoro } from "@/components/pomodoro";
-import History from "@/components/pomodoro/PomodoroList";
 import { getTomatoHistory } from "@/db/tomatoActions";
+import { getFirstDateOfMonth, getLastDateOfMonth } from "@/utils/timeUtils";
+import dayjs from "dayjs";
 import React from "react";
 
 async function Tomato() {
-  const dataSource = await getTomatoHistory({});
+  const today = new Date();
+  const startOfMonth = getFirstDateOfMonth(today);
+  const endOfMonth = getLastDateOfMonth(today);
+  const dataSource = await getTomatoHistory({
+    startTime: startOfMonth,
+    endTime: endOfMonth,
+  });
+  const dateRecord: RecordDate[] = dataSource.map((item) => ({
+    color: "bg-tomato-record",
+    date: item.startAt,
+    dateStr: dayjs(item.startAt).format("YYYY-MM-DD"),
+  }));
 
   return (
-    <Content rightSideBar={<History dataSource={dataSource} />}>
-      <Pomodoro />
-    </Content>
+    <div className="pt-22 pb-14 flex justify-center gap-4">
+      <div>
+        <Calendar recordDate={dateRecord} />
+      </div>
+      <div>
+        <Pomodoro />
+      </div>
+      <div></div>
+    </div>
   );
 }
 

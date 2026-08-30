926e6b6789205877bbeda81262ea994e52f4c2d5
Feline <zxf_tech@outlook.com>
2026-08-30 21:46:04 +0800

    取消生产环境屏蔽 userMenu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,9 @@
-import { FC, ReactNode } from "react";
-import NeuDiv, { neuSurfaceClassNames } from "../NeuDiv";
-import Link from "next/link";
-import { IconNeonCat } from "../Icon/presetIcon";
-import { UserMenu } from "../Profile/UserMenu";
-import Theme from "../Theme";
+import { FC, ReactNode } from 'react';
+import NeuDiv, { neuSurfaceClassNames } from '../NeuDiv';
+import Link from 'next/link';
+import { IconNeonCat } from '../Icon/presetIcon';
+import { UserMenu } from '../Profile/UserMenu';
+import Theme from '../Theme';
 
 interface NavbarProps {
   leftSlot?: ReactNode;
@@ -13,7 +13,6 @@ interface NavbarProps {
 }
 
 const Navbar: FC<NavbarProps> = ({ routeList }) => {
-  const isDev = process.env.NODE_ENV === "development";
   return (
     <NeuDiv className="navbar m-0! fixed top-0 left-0 right-0 flex items-center z-999 neu-light">
       <div className="flex items-center justify-center text-center w-20">
@@ -25,11 +24,11 @@ const Navbar: FC<NavbarProps> = ({ routeList }) => {
             <li key={route}>
               <Link
                 className={neuSurfaceClassNames({
-                  surface: "flat",
-                  interactionEffect: "raise",
-                  className: "block !p-0 mx-0 my-2",
+                  surface: 'flat',
+                  interactionEffect: 'raise',
+                  className: 'block !p-0 mx-0 my-2',
                 })}
-                href={`/${route === "home" ? "" : route}`}
+                href={`/${route === 'home' ? '' : route}`}
               >
                 <span className="block px-4 py-1 mx-2 my-1 font-medium rounded-md navbar-link text-font!">
                   {route.toUpperCase()}
@@ -40,7 +39,9 @@ const Navbar: FC<NavbarProps> = ({ routeList }) => {
         })}
       </ul>
       <Theme />
-      <div className="w-20">{isDev ? <UserMenu /> : null}</div>
+      <div className="w-20">
+        <UserMenu />
+      </div>
     </NeuDiv>
   );
 };
